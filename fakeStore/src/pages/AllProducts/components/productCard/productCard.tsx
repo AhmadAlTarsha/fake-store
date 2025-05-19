03c8a584f8b70350e69326478d183f71833374fb
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2025-05-19 23:43:41 +0300

    start add routing

--- fakeStore/src/pages/AllProducts/components/productCard/productCard.tsx
+++ fakeStore/src/pages/AllProducts/components/productCard/productCard.tsx
@@ -9,7 +9,10 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ title, price, image }) => {
   return (
-    <div className="product-card">
+    <div onClick={()=>{
+      
+      
+    }} className="product-card">
       <img src={image} alt={title} className="product-card__image" />
       <h4 className="product-card__title">{title}</h4>
       <p className="product-card__price">${price}</p>
